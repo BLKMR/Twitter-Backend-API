1097b202beb99a1816aa102c86d7aea71aa638cf
unknown <xmcbii@gmail.com>
2024-04-22 11:35:04 -0500

    Initial commit / separate branch created

---
+++ src/test/java/com/cooksystems/springassessmentsocialmediaapr2024team3/SpringAssessmentSocialMediaApr2024Team3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooksystems.springassessmentsocialmediaapr2024team3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAssessmentSocialMediaApr2024Team3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
