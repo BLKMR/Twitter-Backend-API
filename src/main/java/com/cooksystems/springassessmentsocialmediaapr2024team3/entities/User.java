b5dfd2599e38cf8f24a7ce35466db8d5a1257807
unknown <xmcbii@gmail.com>
2024-04-23 15:10:31 -0500

    fixed UserRepository

--- src/main/java/com/cooksystems/springassessmentsocialmediaapr2024team3/entities/User.java
+++ src/main/java/com/cooksystems/springassessmentsocialmediaapr2024team3/entities/User.java
@@ -5,6 +5,7 @@ import java.security.Timestamp;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
--- src/main/java/com/cooksystems/springassessmentsocialmediaapr2024team3/repositories/UserRepository.java
+++ src/main/java/com/cooksystems/springassessmentsocialmediaapr2024team3/repositories/UserRepository.java
@@ -1,5 +1,14 @@
 package com.cooksystems.springassessmentsocialmediaapr2024team3.repositories;
 
-public class UserRepository{
+
+import com.cooksystems.springassessmentsocialmediaapr2024team3.entities.User;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
 
 }
